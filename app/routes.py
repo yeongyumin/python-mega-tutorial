15d90ad903550440f92348949a3095e23123cace
yeongyumin <yeongyumin@vuno.co>
2020-01-14 15:45:39 +0900

    feat: add ch3: webforms

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = { 'username': 'Miguel' }
-    print('???')
+    print(app)
+    print(app.config['SECRET_KEY'])
     posts = [
         {
             'author': {'username': 'John'},
@@ -17,3 +19,15 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    # check whether users press the submit button or not
+    # gather all the data and run all the validators attached to fields
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
