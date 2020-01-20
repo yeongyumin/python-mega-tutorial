febb1c1bfc548cc8714de276fce0b057306b774f
yeongyumin <yeongyumin@vuno.co>
2020-01-20 17:33:57 +0900

    feat: add ch6 profile page and avatars

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -9,6 +10,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     # not actual field
     # one side
@@ -16,6 +19,11 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def avatar(self, size):
+        # encode the string as bytes before passing it on to the hash function
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
